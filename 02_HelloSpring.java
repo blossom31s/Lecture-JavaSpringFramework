8c384a5605694234774c81787051da3fe6fd1ec0
blossom31s <44717935+blossom31s@users.noreply.github.com>
2020-03-24 16:18:23 +0900

    Update 02_HelloSpring.java

--- 02_HelloSpring.java
+++ 02_HelloSpring.java
@@ -1,7 +1,9 @@
 /*
 // pom.xml
 
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
   <modelVersion>4.0.0</modelVersion>
   <groupId>springLecture</groupId>
   <artifactId>springTest</artifactId>
