0b6f48decdc3435cf00e126c841cfe1ba3234c9f
blossom31s <44717935+blossom31s@users.noreply.github.com>
2020-03-24 22:49:03 +0900

    Update 01_DI.java

--- 01_DI.java
+++ 01_DI.java
@@ -1,1 +1,34 @@
+/*
+// Project File Structure
+springTest
+  src/main/java
+    ems.member
+      DataBaseConnectionInfo.java
+      Student.java
+    ems.member.assembler
+      StudentAssembler.java
+    ems.member.dao
+      StudentDao.java
+    ems.member.main
+      MainClass.java
+      MainClassUseXML.java
+      MainClassUseXMLs.java
+      MainClassUseXMLsImport.java
+    ems.member.service
+      EMSInformationService.java
+      StudentAllSelectService.java
+      StudentDeleteService.java
+      StudentModifyService.java
+      StudentRegisterService.java
+      StudentSelectService.java
+  src/main/resources
+    appCtx1.xml
+    appCtx2.xml
+    appCtx3.xml
+    appCtxImport.xml
+    applicationContext.xml
+pom.xml
+
+*/
+
 ...
