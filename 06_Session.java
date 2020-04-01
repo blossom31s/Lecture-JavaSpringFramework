3b9d503a663d9f1c0f9dc6231e2157b1f6ff92d5
blossom31s <44717935+blossom31s@users.noreply.github.com>
2020-04-02 07:51:28 +0900

    Update 06_Session.java

--- 06_Session.java
+++ 06_Session.java
@@ -1,4 +1,44 @@
-#
+# ProjectName : practiceSpring
+    Java Resources
+        src/main/java
+            com.cs.practice
+                HomeController.java
+            com.cs.practice.member
+                Member.java
+            com.cs.practice.member.controller
+                MemberController.java
+            com.cs.practice.member.dao
+                IMemberDao.java
+                MemberDao.java
+            com.cs.practice.member.service
+                IMemberService.java
+                MemberService.java
+    src
+        main
+            webapp
+                resources
+                    css
+                        normal.css
+                WEB-INF
+                    spring
+                        appServlet
+                            servlet-context.xml
+                        root-context.xml
+                    views
+                        member
+                            joinForm.jsp
+                            joinOK.jsp
+                            loginForm.jsp
+                            loginOK.jsp
+                            logoutOK.jsp
+                            modifyForm.jsp
+                            modifyOK.jsp
+                            removeForm.jsp
+                            removeOK.jsp
+                        index.jsp
+                    web.xml
+    target
+        pom.xml
 
 /*
 //
